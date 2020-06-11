23eaa0cb836f1d99bc97d7fe6299f953c67e8aaa
Erik Guzman <erik.guzman@guzman.codes>
2020-06-11 13:47:41 -0700

    Add helmet to secure app

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import App from './app'
 
 import * as bodyParser from 'body-parser'
 import loggerMiddleware from './middleware/logger'
+import * as helmet from "helmet";
 
 import PostsController from './controllers/posts/posts.controller'
 import HomeController from './controllers/home/home.controller'
@@ -15,12 +16,9 @@ const app = new App({
     middleWares: [
         bodyParser.json(),
         bodyParser.urlencoded({ extended: true }),
-        loggerMiddleware
+        loggerMiddleware,
+        helmet(),
     ]
 })
 
 app.listen()
-
-// http://rousseau-alexandre.fr/en/programming/2019/06/19/express-typescript.html
-// https://wanago.io/2018/12/03/typescript-express-tutorial-routing-controllers-middleware/
-// https://www.tutorialsteacher.com/typescript
